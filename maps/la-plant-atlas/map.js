51351ab8128c268d5f60c3667404ab03bc62b28c
maxogden <max@maxogden.com>
2024-09-23 23:14:37 -0700

    clean up

--- maps/la-plant-atlas/map.js
+++ maps/la-plant-atlas/map.js
@@ -5,7 +5,15 @@ const map = new maplibregl.Map({
   center: [-118.2094, 34.2303],
   zoom: 9,
   hash: true,
+  minPitch: 0,
+  maxPitch: 85,
 });
+
+var control = new maplibreSearchBox.MapLibreSearchControl({
+  useMapFocusPoint: true,
+});
+
+map.addControl(control, "top-right");
 
 map.addControl(new maplibregl.FullscreenControl());
 
@@ -15,20 +23,22 @@ var styles = (MapLibreStyleDefinition = [
     uri: "https://api.maptiler.com/maps/hybrid/style.json?key=" + token,
   },
   {
-    title: "Basic",
+    title: "Streets",
     uri: "https://api.maptiler.com/maps/streets/style.json?key=" + token,
   },
   {
-    title: "Light",
+    title: "Bright",
     uri: "https://api.maptiler.com/maps/bright/style.json?key=" + token,
   },
   {
-    title: "Outdoors",
+    title: "Outdoor",
     uri: "https://api.maptiler.com/maps/outdoor/style.json?key=" + token,
   },
 ]);
 
-// map.addControl(new MapLibreStyleSwitcherControl(styles, "Satellite Hybrid"));
+var loadedStyle;
+
+map.addControl(new MapLibreStyleSwitcherControl(styles, "Satellite Hybrid"));
 
 let hoveredGridID = null;
 
@@ -63,21 +73,32 @@ function toggleSidebar(id) {
   elem.className = classes.join(" ");
 }
 
-map.on("load", async () => {
-  map.addSource("terrain", {
-    type: "raster-dem",
-    url: "https://api.maptiler.com/tiles/terrain-rgb-v2/tiles.json?key=VK9en7fqcN8VTzfv5LVe",
-  });
+map.once("load", () => {
+  toggleSidebar("left");
+});
 
-  map.setTerrain({
-    source: "terrain",
-    exagerration: 2.5,
-  });
+map.on("styledata", (e) => {
+  var loaded = e.style.stylesheet.name;
+  if (loaded !== loadedStyle) {
+    loadedStyle = loaded;
+    if (loaded === "Satellite Hybrid") {
+      map.addSource("terrain", {
+        type: "raster-dem",
+        url: "https://api.maptiler.com/tiles/terrain-rgb-v2/tiles.json?key=VK9en7fqcN8VTzfv5LVe",
+      });
 
+      map.setTerrain({
+        source: "terrain",
+        exagerration: 2.5,
+      });
+    }
+    onLoad();
+  }
+});
+
+var onLoad = async () => {
   var content = document.querySelector(".content");
   content.innerHTML = marked.parse(content.innerHTML);
-
-  toggleSidebar("left");
 
   map.addSource("inat", {
     type: "raster",
@@ -94,10 +115,8 @@ map.on("load", async () => {
       "raster-opacity": 0.7,
     },
   });
-});
 
-map.on("load", async () => {
-  const url = "./lagrid.json";
+  const url = "./lacountyboundary.json";
   var json;
   try {
     const response = await fetch(url);
@@ -110,9 +129,38 @@ map.on("load", async () => {
     console.error(error.message);
   }
 
-  map.addSource("grid", {
+  map.addSource("county", {
     type: "geojson",
     data: json,
+  });
+
+  map.addLayer({
+    id: "countyborder",
+    type: "line",
+    source: "county",
+    layout: {},
+    paint: {
+      "line-color": "#80a868",
+      "line-width": 3,
+    },
+  });
+
+  const url2 = "./lagrid.json";
+  var json2;
+  try {
+    const response = await fetch(url2);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    json2 = await response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  map.addSource("grid", {
+    type: "geojson",
+    data: json2,
   });
 
   map.addLayer({
@@ -125,7 +173,7 @@ map.on("load", async () => {
       "fill-opacity": [
         "case",
         ["boolean", ["feature-state", "hover"], false],
-        0.3,
+        0,
         0,
       ],
     },
@@ -161,7 +209,7 @@ map.on("load", async () => {
       "text-halo-width": 2,
     },
   });
-});
+};
 
 // When the user moves their mouse over the state-fill layer, we'll update the
 // feature state for the feature under the mouse.
@@ -170,13 +218,16 @@ map.on("mousemove", "grid-hover", (e) => {
     if (hoveredGridID) {
       map.setFeatureState(
         { source: "grid", id: hoveredGridID },
-        { hover: false }
+        { clicked: false }
       );
     }
     hoveredGridID = e.features[0].id;
     if (hoveredGridID === polygonID) return;
 
-    map.setFeatureState({ source: "grid", id: hoveredGridID }, { hover: true });
+    map.setFeatureState(
+      { source: "grid", id: hoveredGridID },
+      { clicked: true }
+    );
   }
 });
 
@@ -202,26 +253,8 @@ map.on("click", "grid-hover", async (e) => {
     var content = document.querySelector(".content");
     content.innerHTML = marked.parse('<span class="loader"></span>');
 
-    if (polygonID) {
-      map.removeFeatureState({
-        source: "grid",
-        id: polygonID,
-      });
-    }
-
-    clearHover();
-
     polygonID = e.features[0].id;
 
-    map.setFeatureState(
-      {
-        source: "grid",
-        id: polygonID,
-      },
-      {
-        clicked: true,
-      }
-    );
     var ul = e.features[0].geometry.coordinates[0][0];
     var lr = e.features[0].geometry.coordinates[0][2];
     var center = [ul[0] + (lr[0] - ul[0]) / 2, ul[1] + (lr[1] - ul[1]) / 2];
@@ -230,8 +263,8 @@ map.on("click", "grid-hover", async (e) => {
       center: center, // Fly to the selected target
       essential: true, // This animation is considered essential with
       zoom: 12.5,
-      //respect to prefers-reduced-motion
     });
+
     var rect = e.features[0].geometry.coordinates[0];
     var species = await getSpecies(rect);
     var iNatExplore = `https://www.inaturalist.org/observations?verifiable=true&spam=false&project_id=calflora-native-plants-of-california&geoprivacy=open&taxon_geoprivacy=open&quality_grade=research&swlng=${rect[3][0]}&swlat=${rect[3][1]}&nelng=${rect[1][0]}&nelat=${rect[1][1]}&locale=en&preferred_place_id=14`;
@@ -257,35 +290,6 @@ Species count: ${species.total_results} (<a href="${iNatExplore}" target="_blank
     });
     content.innerHTML = marked.parse(tmp);
   }
-});
-
-map.on("load", async () => {
-  const url = "./lacountyboundary.json";
-  var json;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    json = await response.json();
-  } catch (error) {
-    console.error(error.message);
-  }
-  map.addSource("county", {
-    type: "geojson",
-    data: json,
-  });
-  map.addLayer({
-    id: "countyborder",
-    type: "line",
-    source: "county",
-    layout: {},
-    paint: {
-      "line-color": "#80a868",
-      "line-width": 3,
-    },
-  });
 });
 
 var getSpecies = async function (rect) {
