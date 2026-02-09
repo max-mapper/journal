959a29cb73091a97b54182d60595676fa8032503
maxogden <max@maxogden.com>
2026-02-09 22:08:24 +0900

    update

---
+++ jp-words/makejpjson.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+var dict = require("./jmdict-eng-common-3.6.2.json");
+
+var simple = { index: {}, defs: [] };
+for (var x of dict.words) {
+  var keys = [];
+  for (var kanji of x.kanji) {
+    if (kanji.common) keys.push(kanji.text);
+  }
+  for (var kana of x.kana) {
+    if (kana.common) keys.push(kana.text);
+  }
+  var val = x.sense.map((s) => {
+    return {
+      info: s.info,
+      gloss: s.gloss.map((g) => {
+        return g.text;
+      }),
+    };
+  });
+
+  simple.defs.push(val);
+  var idx = simple.defs.length - 1;
+  for (var k of keys) {
+    if (!simple.index[k]) simple.index[k] = [];
+    simple.index[k].push(idx);
+  }
+}
+
+fs.writeFileSync("./jpdict.json", JSON.stringify(simple));
